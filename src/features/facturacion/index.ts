b5da21f824253536baa7d107f87e6a9cc7ebdddf
Pol Gubau Amores <gubaupol@gmail.com>
2026-09-09 10:03:42 +0200

    Remove unused exports and streamline index file
    This commit cleans up the index.ts file by removing unnecessary exports related to provisional onboardings and various utility functions. The focus is now on exporting the application module and the PersistentFacturacionPage component, enhancing clarity and maintainability.
    
    Issue: MW-0000
    Type: refactor

--- src/features/facturacion/index.ts
+++ src/features/facturacion/index.ts
@@ -1,22 +1,2 @@
-export {
-  formatCurrency,
-  formatDate,
-  useDescartarBorradorFactura,
-  useEmitirFactura,
-  useFacturas,
-  useGuardarBorradorFactura,
-  useRectificarFactura,
-  useRegistrarCobroFactura,
-} from './infrastructure/supabase-facturas'
-export * from './application/factura-types'
-export * from './application/factura-ciclo'
+export * from './application'
 export { PersistentFacturacionPage } from './ui/persistent-facturacion-page'
-export {
-  etiquetaEstadoEconomicoProvisional,
-  onboardingEconomicoProvisional,
-  resumenEconomicoProvisional,
-} from './application/provisional-onboardings'
-export type {
-  EstadoEconomicoProvisional,
-  OnboardingEconomicoProvisional,
-} from './application/provisional-onboardings'
